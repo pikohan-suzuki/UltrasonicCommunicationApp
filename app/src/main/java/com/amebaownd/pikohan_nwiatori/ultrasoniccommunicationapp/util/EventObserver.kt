cec583df71166d792536ea5828e8e396a86940db
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-08 23:57:25 +0900

    EventObserver

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/util/EventObserver.kt
@@ -0,0 +1,25 @@
+package com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.util
+
+import androidx.lifecycle.Observer
+
+class Event<out T>(private val context:T){
+    var hasHandled = false
+        private set
+    fun getContextIfNotHandled():T? {
+        return if (hasHandled) {
+            null
+        } else {
+            hasHandled = true
+            context
+        }
+    }
+}
+
+class EventObserver<T>(private val onEventUnhandledContext:(T)->Unit) : Observer<Event<T>>{
+    override fun onChanged(event: Event<T>?){
+        event?.getContextIfNotHandled()?.let{
+            onEventUnhandledContext(it)
+        }
+    }
+
+}
