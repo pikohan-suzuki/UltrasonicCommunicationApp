dd3534622051d66ba84221dc3e7f9a94fff6acd5
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-09 16:24:56 +0900

    receiveViewModel

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/receive/ReceiveViewModel.kt
@@ -0,0 +1,8 @@
+package com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.receive
+
+import androidx.lifecycle.ViewModel
+import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.data.repository.Repository
+
+class ReceiveViewModel(repository: Repository) : ViewModel(){
+
+}
