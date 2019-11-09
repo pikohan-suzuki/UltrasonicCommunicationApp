b784b3f697c071ba0a0d4f8df77461647baa1084
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-09 16:24:47 +0900

    sendFragment

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/send/SendFragment.kt
@@ -0,0 +1,43 @@
+package com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.send
+
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.databinding.FragmentReceiveBinding
+import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.databinding.FragmentSendBinding
+import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.util.getViewModelFactory
+
+class SendFragment :Fragment(){
+
+    private val viewModel :SendViewModel by viewModels { getViewModelFactory() }
+
+    private lateinit var fragmentSendBinding: FragmentSendBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentSendBinding = FragmentSendBinding.inflate(inflater,container,false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel =this@SendFragment.viewModel
+        }
+        return fragmentSendBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        onSendEvent()
+    }
+
+    private fun onSendEvent(){
+
+    }
+}
