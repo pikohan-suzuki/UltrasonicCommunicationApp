5710613e3ab3723091c1f155bd1fd07c396850d0
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-10 01:26:00 +0900

    change_frequency_data

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/send/SoundGenerator.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/send/SoundGenerator.kt
@@ -1,6 +1,7 @@
 package com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.send
 
 import android.util.Log
+import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.data.Constant
 import java.lang.IllegalArgumentException
 
 class SoundGenerator(private val soundHexData: List<Int>) {
@@ -23,23 +24,39 @@ class SoundGenerator(private val soundHexData: List<Int>) {
 
     private fun getHz(hex: Int): Int {
         return when (hex) {
-            0 -> 262
-            1 -> 294
-            2 -> 330
-            3 -> 349
-            4 -> 392
-            5 -> 440
-            6 -> 493
-            7 -> 494
-            8 -> 523
-            9 -> 587
-            10 -> 659
-            11 -> 698
-            12 -> 784
-            13 -> 880
-            14 -> 988
-            15 -> 1047
-            16 -> 1175
+//            0 -> 262
+//            1 -> 294
+//            2 -> 330
+//            3 -> 349
+//            4 -> 392
+//            5 -> 440
+//            6 -> 493
+//            7 -> 494
+//            8 -> 523
+//            9 -> 587
+//            10 -> 659
+//            11 -> 698
+//            12 -> 784
+//            13 -> 880
+//            14 -> 988
+//            15 -> 1047
+            0 -> 5000
+            1 -> 5500
+            2 -> 6000
+            3 -> 6500
+            4 -> 7000
+            5 -> 7500
+            6 -> 8000
+            7 -> 8500
+            8 -> 9000
+            9 -> 9500
+            10 -> 10000
+            11 -> 10500
+            12 -> 11000
+            13 -> 11500
+            14 -> 12000
+            15 -> 12500
+            Constant.SPACE  ->  15000
             else ->
                 throw IllegalArgumentException("Get not HEX data ${hex}")
         }
