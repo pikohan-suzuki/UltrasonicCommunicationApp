454e920fdc5373f591a535698f6c00133c77c358
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-09 16:25:11 +0900

    add navigation setting

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/MainActivity.kt
@@ -2,11 +2,32 @@ package com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration=
+            AppBarConfiguration.Builder(R.id.sendFragment).build()
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
