f9c3ac98e356786698ec4db6999a9b04cff9ced7
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-10 01:25:04 +0900

    定数データ

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/data/Constant.kt
@@ -0,0 +1,8 @@
+package com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.data
+
+class Constant {
+    companion object{
+        const val SOUND_LENGTH = 250
+        const val SPACE = 64
+    }
+}
