57f18c6b2c9872d3c98faa12b5a98e4c4976623f
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-10 22:37:23 +0900

    add viewModel

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/receive/ReceiveFragment.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/ultrasoniccommunicationapp/receive/ReceiveFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.databinding.FragmentSendBinding
+import androidx.fragment.app.viewModels
 import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.databinding.FragmentReceiveBinding
+import com.amebaownd.pikohan_nwiatori.ultrasoniccommunicationapp.util.getViewModelFactory
 
 class ReceiveFragment :Fragment(){
+
+    private val viewModel:ReceiveViewModel by viewModels{getViewModelFactory()}
 
     private lateinit var fragmentReceiveBinding: FragmentReceiveBinding
 
@@ -19,6 +22,7 @@ class ReceiveFragment :Fragment(){
     ): View? {
         fragmentReceiveBinding = FragmentReceiveBinding.inflate(inflater,container,false).apply {
             lifecycleOwner = viewLifecycleOwner
+            viewModel = this@ReceiveFragment.viewModel
         }
         return fragmentReceiveBinding.root
     }
